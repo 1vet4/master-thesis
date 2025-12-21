0384593389606b4715a788efdef6648218d92347
1vet4 <113985634+1vet4@users.noreply.github.com>
2025-12-21 12:36:53 +0200

    Add files via upload

---
+++ code/constants.py
@@ -0,0 +1,123 @@
+FUNDING_FILES_LIST = [
+    "/FINANSAVIMAS_2016.xlsx",
+    "/FINANSAVIMAS_2017.xlsx",
+    "/FINANSAVIMAS_2018.xlsx",
+    "/FINANSAVIMAS_2019.xlsx",
+    "/FINANSAVIMAS_2020.xlsx",
+    "/FINANSAVIMAS_2021.xlsx",
+    "/FINANSAVIMAS_2022.xlsx",
+    "/FINANSAVIMAS_2023.xlsx",
+    "/FINANSAVIMAS_2024.xlsx",
+    "/FINANSAVIMAS_2025.xlsx",
+    "/FINANSAVIMAS_2026.xlsx",
+    "/FINANSAVIMAS_2027.xlsx",
+]
+ENROLLMENT_FILES_LIST = [
+    "/LAMA_BPO_2016.xlsx",
+    "/LAMA_BPO_2017.xlsx",
+    "/LAMA_BPO_2018.xlsx",
+    "/LAMA_BPO_2019.xlsx",
+    "/LAMA_BPO_2020.xlsx",
+    "/LAMA_BPO_2021.xlsx",
+    "/LAMA_BPO_2022.xlsx",
+    "/LAMA_BPO_2023.xlsx",
+    "/LAMA_BPO_2024.xlsx",
+    "/LAMA_BPO_2025.xlsx",
+]
+UNIVERSITIES = [
+    "Kauno technologijos universitetas",
+    "Klaipėdos universitetas",
+    "Lietuvos muzikos ir teatro akademija",
+    "Lietuvos sporto universitetas",
+    "Lietuvos sveikatos mokslų universitetas",
+    "Mykolo Romerio universitetas",
+    "Vilniaus dailės akademija",
+    "Vilniaus Gedimino technikos universitetas",
+    "Vilniaus universitetas",
+    "Vytauto Didžiojo universitetas"
+]
+UNIVERSITIES_CODES = [
+    "KTU",
+    "KU",
+    "LMTA",
+    "LSMU",
+    "LSU",
+    "MRU",
+    "VDA",
+    "VDU",
+    "VGTU",
+    "VU"
+]
+UNIVERSITIES_CODES_MAPPING = {
+    "kauno technologijos universitetas": "KTU",
+    "klaipėdos universitetas": "KU",
+    "lietuvos muzikos ir teatro akademija": "LMTA",
+    "lietuvos sporto universitetas": "LSU",
+    "lietuvos sveikatos mokslų universitetas": "LSMU",
+    "mykolo romerio universitetas": "MRU",
+    "vilniaus dailės akademija": "VDA",
+    "vilniaus gedimino technikos universitetas": "VGTU",
+    "vilniaus universitetas": "VU",
+    "vytauto didžiojo universitetas": "VDU"
+}
+BASE_TUITION_FEES = [
+    "/NORMINES_KAINOS_2016.xlsx",
+    "/NORMINES_KAINOS_2017.xlsx",
+    "/NORMINES_KAINOS_2018.xlsx",
+    "/NORMINES_KAINOS_2019.xlsx",
+    "/NORMINES_KAINOS_2020.xlsx",
+    "/NORMINES_KAINOS_2021.xlsx",
+    "/NORMINES_KAINOS_2022.xlsx",
+    "/NORMINES_KAINOS_2023.xlsx",
+    "/NORMINES_KAINOS_2024.xlsx",
+    "/NORMINES_KAINOS_2025.xlsx",
+]
+GRADUATES_FILES_LIST = [
+    "KTU.xlsx",
+    "KU.xlsx",
+    "LMTA.xlsx",
+    "LSU.xlsx",
+    "LSMU.xlsx",
+    "MRU.xlsx",
+    "VDA.xlsx",
+    "VDU.xlsx",
+    "VU.xlsx",
+    "VGTU.xlsx",
+]
+LEFT_FOR_PART_TIME_STUDIES_FILES_LIST = [
+    "KTU.xlsx",
+    "KU.xlsx",
+    "LMTA.xlsx",
+    "LSU.xlsx",
+    "LSMU.xlsx",
+    "MRU.xlsx",
+    "VDA.xlsx",
+    "VDU.xlsx",
+    "VU.xlsx",
+    "VGTU.xlsx",
+]
+NUMBER_OF_STUDENTS_FILES_LIST = [
+    "/STUDENTU_SKAICIUS_2016.xlsx",
+    "/STUDENTU_SKAICIUS_2017.xlsx",
+    "/STUDENTU_SKAICIUS_2018.xlsx",
+    "/STUDENTU_SKAICIUS_2019.xlsx",
+    "/STUDENTU_SKAICIUS_2020.xlsx",
+    "/STUDENTU_SKAICIUS_2021.xlsx",
+    "/STUDENTU_SKAICIUS_2022.xlsx",
+    "/STUDENTU_SKAICIUS_2023.xlsx",
+    "/STUDENTU_SKAICIUS_2024.xlsx",
+    "/STUDENTU_SKAICIUS_2025.xlsx",
+]
+
+EMPLOYEES_FILES_LIST = [
+    "KTU.xlsx",
+    "KU.xlsx",
+    "LMTA.xlsx",
+    "LSU.xlsx",
+    "LSMU.xlsx",
+    "MRU.xlsx",
+    "VDA.xlsx",
+    "VDU.xlsx",
+    "VU.xlsx",
+    "VGTU.xlsx",
+]
